b75b07a267bf31c0b830cb874db14ddcbcc8caba
Emmanuel Munoz <em66@cs.washington.edu>
2019-06-02 21:33:53 -0700

    Fixed SQL injection with prepareStatement

--- contactform.php
+++ contactform.php
@@ -9,9 +9,18 @@ if(isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['email']) &&
     $phone = $_POST['phone'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $query_string = "SELECT * FROM contact WHERE email = '$email'";
+    //$query_string = "SELECT * FROM contact WHERE email = '$email'";
     try {
-      $rows = $db->query($query_string);
+      $query_string = "INSERT INTO contact(name, phone, email, message) values(:name, :phone, :email, :message);";
+      $stmt = $db->prepare($query_string);
+
+      # create the associative array between the variables in the
+      # insert SQL statement with the actual values
+      $params = array("name" => $name,
+               "email" => $email,
+               "phone" => $phone,
+               "message" => $message);
+      $stmt->execute($params);
     }
     catch(PDOException $ex) {
       die("There has been a database error.");
@@ -22,43 +31,43 @@ if(isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['email']) &&
   header("Content-type: text/plain");
   die("I need a valid email, phone, message, and name.");
 }
-
-$result = mysqli_query($con, $sql);
-
-$row = mysqli_fetch_array($result);
-
-if(($row['message']==$message) && ($row['email']==$email)){
-    ?>
-    <script>
-        confirm('I have already recieved your message, Enter a New One.');
-        window.location = "contact.php";
-    </script>
-
-    <?php
-}
-
-else{
-    $data = "INSERT INTO contact(name, phone, email,
-    message) values('$name', '$phone', '$email', '$message')";
-
-    if(mysqli_query($con, $data)){
-        ?>
-            <script>
-                confirm('Message Recieved. <?php echo $name ?>, Thank you!');
-                window.location = "contact.php";
-            </script>
-
-        <?php
-    }
-    else{
-        ?>
-            <script>
-                alert('Error, Try Again!');
-                window location = "contact.php";
-            </script>
-        <?php
-    }
-}
+//
+// $result = mysqli_query($con, $sql);
+//
+// $row = mysqli_fetch_array($result);
+//
+// if(($row['message']==$message) && ($row['email']==$email)){
+//     ?>
+//     <script>
+//         confirm('I have already recieved your message, Enter a New One.');
+//         window.location = "contact.php";
+//     </script>
+//
+//     <?php
+// }
+//
+// else{
+//     $data = "INSERT INTO contact(name, phone, email,
+//     message) values('$name', '$phone', '$email', '$message')";
+//
+//     if(mysqli_query($con, $data)){
+//         ?>
+//             <script>
+//                 confirm('Message Recieved. <?php echo $name ?>, Thank you!');
+//                 window.location = "contact.php";
+//             </script>
+//
+//         <?php
+//     }
+//     else{
+//         ?>
+//             <script>
+//                 alert('Error, Try Again!');
+//                 window location = "contact.php";
+//             </script>
+//         <?php
+//     }
+// }
 
 function is_valid_mode($mode) {
   $VALID_MODES = array();
